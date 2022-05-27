bd91c8fa4570fbe08e66b9a54ab5023ea502c83c
Liam <liamwouters4@gmail.com>
2022-05-27 23:14:57 +0200

    Start Stemming

--- Word.cpp
+++ Word.cpp
@@ -4,9 +4,44 @@
 
 #include "Word.h"
 
+/// Private Functions ///
+bool Word::isVowel(char* c) {
+    return (*c == 'a' || *c == 'e' || *c == 'i' || *c == 'o' || *c == 'u');
+}
+
+bool Word::isConsonant(string word, int index) {
+    // a consonant is a letter that is not a vowel, and other than Y preceded by a consonant
+    bool b = isVowel(&word[index]);
+    if (b == false) {
+        // is not vowel, check for Y
+        if (word[index] == 'y') {
+            if (index != 0) {
+                return !isConsonant(word, index-1); // check if letter before Y is not a consonant;
+            }
+            else {return true;}
+        }
+        else {return true;}
+    }
+    else {return false;}
+}
+
+bool Word::changeSuffix(string& word, string S1, string S2) {
+    if (word.length() < S1.length()) {return false;}
+    if (word.substr(word.length()-S1.length(), word.length()) == S1) {
+        word = word.erase(word.length()-S1.length(), word.length());
+        word += S2;
+        return true;
+    }
+    return false;
+}
+
+/////////////////////////
+/// Public Functions ///
+
 Word::Word(const string &token) : token(token) {
     specialChar = false;
     abbreviation = false;
+    root = Stem();
 }
 
 string Word::getString() {
@@ -27,4 +62,44 @@ void Word::setAbbreviation() {
 
 bool Word::isAbbreviation() {
     return abbreviation;
+}
+
+string Word::Stem() {
+    /*
+     * The porter algorithm for suffix trimming (stemming)
+     * source: http://facweb.cs.depaul.edu/mobasher/classes/csc575/papers/porter-algorithm.html
+     */
+    string rootForm;
+    bool skip = false; // bool used to skip parts if necessary
+
+    cout << token << " -> ";
+    // SSES -> SS
+    if (!skip) {
+        skip = changeSuffix(token, "sses", "ss");
+    }
+    // IES -> I
+    if (!skip) {
+        skip = changeSuffix(token, "ies", "i");
+    }
+    // SS -> SS
+    if (!skip) {
+        skip = changeSuffix(token, "ss", "ss");
+    }
+    // S ->
+    if (!skip) {
+        skip = changeSuffix(token, "s", "");
+    }
+    cout << token << endl;
+
+    skip = false; // reset bool
+
+    // (m>0) EED -> EE
+    if (true) {
+        changeSuffix(token, "eed", "ee");
+    }
+    // (*v*) ED ->
+
+    // (*v*) ING ->
+
+    return rootForm;
 }
--- Word.h
+++ Word.h
@@ -11,9 +11,13 @@ using namespace std;
 class Word {
 private:
     string token;
+    string root;
 
     bool specialChar; // is een special character: punt, komma, ...
     bool abbreviation; // is een afkorting vb: "Dr."
+    bool isVowel(char* c);
+    bool isConsonant(string word, int index);
+    bool changeSuffix(string& word, string S1, string S2);
 public:
     Word(const string &token);
 
@@ -22,6 +26,8 @@ public:
     bool isSpecialChar();
     void setAbbreviation();
     bool isAbbreviation();
+
+    string Stem();
 };
 
 #endif //TATEXTEDITOR_WORD_H
